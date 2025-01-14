b9f4ddd6d752c2ea2672dbef77b864d064b423a9
adithyagenie <adithyagenie@gmail.com>
2025-01-14 12:56:03 +0530

    Bad request no longer returns body
    - Removed error body from 400 requests
    
    Signed-off-by: adithyagenie <adithyagenie@gmail.com>

--- src/api/admin/mod.rs
+++ src/api/admin/mod.rs
@@ -1,10 +1,19 @@
-use actix_web::web;
+use actix_web::{web, HttpResponse};
 
 pub mod menu;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/menu")
+            .app_data(web::JsonConfig::default().error_handler(|err, _| {
+                info!("Error in admin menu: {}", err);
+                actix_web::error::InternalError::from_response(
+                    "",
+                    HttpResponse::BadRequest()
+                        .finish(),
+                )
+                    .into()
+            }))
             .route("/items", web::get().to(menu::get_all_menu_items))
             .route("/item", web::get().to(menu::get_menu_item))
             .route("/create", web::put().to(menu::create_menu_item))
--- src/api/users/mod.rs
+++ src/api/users/mod.rs
@@ -1,10 +1,19 @@
-use actix_web::web;
+use actix_web::{web, HttpResponse};
 
 pub mod account;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/auth")
+            .app_data(web::JsonConfig::default().error_handler(|err, _| {
+                info!("Error in user auth: {}", err);
+                actix_web::error::InternalError::from_response(
+                    "",
+                    HttpResponse::BadRequest()
+                        .finish(),
+                )
+                .into()
+            }))
             .route("/login", web::post().to(account::login))
             .route("/create_user", web::post().to(account::create_user))
     );
