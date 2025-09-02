Project-xS/xs-backend

commit b3f7ddf4b0fd534c676c07f30c546c3dd73697a6

prefetch asset urls
- pic link used to be a bool in api schema, it is now a string
- pic link is now fetched within the single api call itself, reducing frontend's load and preventing hammering of backend for assets
- plans to introduce checksum based caching policy for frontend to cache images more efficiently.

Signed-off-by: adithyagenie <adithyagenie@gmail.com>

src/db/admin/menu.rs
@@ -1,20 +1,25 @@
 use crate::db::errors::RepositoryError;
 use crate::db::schema::menu_items::dsl::*;
-use crate::db::DbConnection;
+use crate::db::{AssetOperations, DbConnection};
+use crate::enums::admin::MenuItemWithPic;
 use crate::models::admin::{MenuItem, NewMenuItem, UpdateMenuItem};
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::result::Error;
-
+use futures::future::join_all;
 use log::error;
 
 pub struct MenuOperations {
     pool: Pool<ConnectionManager<PgConnection>>,
+    asset_ops: AssetOperations,
 }
 
 impl MenuOperations {
-    pub fn new(pool: Pool<ConnectionManager<PgConnection>>) -> Self {
-        Self { pool }
+    pub async fn new(pool: Pool<ConnectionManager<PgConnection>>) -> Self {
+        Self {
+            pool,
+            asset_ops: AssetOperations::new().await.unwrap(),
+        }
     }
 
     pub fn add_menu_item(&self, menu_item: NewMenuItem) -> Result<MenuItem, RepositoryError> {
@@ -109,22 +114,36 @@ impl MenuOperations {
             })
     }
 
-    pub fn get_all_menu_items(&self) -> Result<Vec<MenuItem>, RepositoryError> {
+    pub async fn get_all_menu_items(&self) -> Result<Vec<MenuItemWithPic>, RepositoryError> {
         let mut conn = DbConnection::new(&self.pool).map_err(|e| {
             error!("get_all_menu_items: failed to acquire DB connection: {}", e);
             e
         })?;
 
-        menu_items
+        let items = menu_items
             .order_by(item_id.asc())
             .load::<MenuItem>(conn.connection())
             .map_err(|e| {
                 error!("get_all_menu_items: error fetching menu items: {}", e);
                 RepositoryError::DatabaseError(e)
-            })
+            })?;
+
+        let futures = items.iter().map(async |item| {
+            let mut item_with_pic: MenuItemWithPic = item.into();
+            if item.pic_link {
+                let pic_url = self.asset_ops.get_object(&item.item_id).await.ok();
+                item_with_pic.pic_link = pic_url;
+                item_with_pic
+            } else {
+                item_with_pic
+            }
+        });
+
+        let results = join_all(futures).await;
+        Ok(results)
     }
 
-    pub fn get_menu_item(&self, itemid: i32) -> Result<MenuItem, RepositoryError> {
+    pub async fn get_menu_item(&self, itemid: i32) -> Result<MenuItemWithPic, RepositoryError> {
         let mut conn = DbConnection::new(&self.pool).map_err(|e| {
             error!(
                 "get_menu_item: failed to acquire DB connection for id {}: {}",
@@ -133,10 +152,9 @@ impl MenuOperations {
             e
         })?;
 
-        menu_items
+        let item = menu_items
             .filter(item_id.eq(itemid))
-            .limit(1)
-            .get_result(conn.connection())
+            .first::<MenuItem>(conn.connection())
             .map_err(|e| {
                 error!(
                     "get_menu_item: error fetching menu item with id {}: {}",
@@ -146,7 +164,16 @@ impl MenuOperations {
                     Error::NotFound => RepositoryError::NotFound(format!("menu_items: {itemid}")),
                     other => RepositoryError::DatabaseError(other),
                 }
-            })
+            })?;
+
+        let mut item_with_pic: MenuItemWithPic = (&item).into();
+        if item.pic_link {
+            let pic_url = self.asset_ops.get_object(&item.item_id).await.ok();
+            item_with_pic.pic_link = pic_url;
+            Ok(item_with_pic)
+        } else {
+            Ok(item_with_pic)
+        }
     }
 }
 
@@ -154,6 +181,7 @@ impl Clone for MenuOperations {
     fn clone(&self) -> Self {
         Self {
             pool: self.pool.clone(),
+            asset_ops: self.asset_ops.clone(),
         }
     }
 }

src/main.rs
@@ -9,7 +9,7 @@ mod models;
 
 use crate::api::default_error_handler;
 use crate::db::{
-    establish_connection_pool, run_db_migrations, AssetUploadOperations, CanteenOperations,
+    establish_connection_pool, run_db_migrations, AssetOperations, CanteenOperations,
     MenuOperations, OrderOperations, SearchOperations, UserOperations,
 };
 use actix_web::{middleware, web, App, HttpServer};
@@ -25,7 +25,7 @@ pub(crate) struct AppState {
     canteen_ops: CanteenOperations,
     order_ops: OrderOperations,
     search_ops: SearchOperations,
-    asset_ops: AssetUploadOperations,
+    asset_ops: AssetOperations,
 }
 
 impl AppState {
@@ -33,11 +33,11 @@ impl AppState {
         let db = establish_connection_pool(url);
         run_db_migrations(db.clone()).expect("Unable to run migrations");
         let user_ops = UserOperations::new(db.clone());
-        let menu_ops = MenuOperations::new(db.clone());
-        let canteen_ops = CanteenOperations::new(db.clone());
+        let menu_ops = MenuOperations::new(db.clone()).await;
+        let canteen_ops = CanteenOperations::new(db.clone()).await;
         let order_ops = OrderOperations::new(db.clone());
-        let search_ops = SearchOperations::new(db.clone());
-        let asset_ops = AssetUploadOperations::new()
+        let search_ops = SearchOperations::new(db.clone()).await;
+        let asset_ops = AssetOperations::new()
             .await
             .expect("Unable to create asset_upload operations");
         AppState {

src/api/admin/menu.rs
@@ -1,8 +1,9 @@
 use crate::db::MenuOperations;
 use crate::enums::admin::{
-    AllItemsResponse, CreateMenuItemResponse, GeneralMenuResponse, ItemResponse, UpdateItemRequest,
+    AllItemsResponse, CreateMenuItemResponse, GeneralMenuResponse, ItemResponse, MenuItemWithPic,
+    UpdateItemRequest,
 };
-use crate::models::admin::{MenuItem, NewMenuItem};
+use crate::models::admin::NewMenuItem;
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 use log::{debug, error};
 
@@ -187,7 +188,7 @@ pub(super) async fn update_menu_item(
 pub(super) async fn get_all_menu_items(
     menu_ops: web::Data<MenuOperations>,
 ) -> actix_web::Result<impl Responder> {
-    let result = web::block(move || menu_ops.get_all_menu_items()).await?;
+    let result = menu_ops.get_all_menu_items().await;
     match result {
         Ok(x) => {
             debug!(
@@ -228,7 +229,7 @@ pub(super) async fn get_menu_item(
     path: web::Path<(i32,)>,
 ) -> actix_web::Result<impl Responder> {
     let req_data = path.into_inner().0;
-    let result = web::block(move || menu_ops.get_menu_item(req_data)).await?;
+    let result = menu_ops.get_menu_item(req_data).await;
     match result {
         Ok(x) => {
             debug!("get_menu_item: successfully fetched menu item '{}'", x.name);
@@ -245,7 +246,7 @@ pub(super) async fn get_menu_item(
             );
             Ok(HttpResponse::BadRequest().json(ItemResponse {
                 status: "error".to_string(),
-                data: MenuItem::default(),
+                data: MenuItemWithPic::default(),
                 error: Some(e.to_string()),
             }))
         }

Cargo.toml
@@ -9,6 +9,7 @@ path = "src/main.rs"
 
 [dependencies]
 actix-web = "4.10.2"
+futures = "0.3.31"
 utoipa-actix-web = "0.1.2"
 utoipa = { version = "5.3.1", features = ["actix_extras"] }
 utoipa-swagger-ui = { version = "9.0.0", features = ["actix-web"] }

src/api/admin/asset_management.rs
@@ -1,4 +1,4 @@
-use crate::db::{AssetUploadOperations, S3Error};
+use crate::db::{AssetOperations, S3Error};
 use crate::enums::admin::ItemUploadResponse;
 use actix_web::{get, post, web, HttpResponse, Responder};
 
@@ -13,7 +13,7 @@ use actix_web::{get, post, web, HttpResponse, Responder};
 )]
 #[post("/upload/{item_id}")]
 pub async fn upload_image_handler(
-    asset_ops: web::Data<AssetUploadOperations>,
+    asset_ops: web::Data<AssetOperations>,
     path: web::Path<(i32,)>,
 ) -> impl Responder {
     let s3_key = path.into_inner().0;
@@ -60,7 +60,7 @@ pub async fn upload_image_handler(
 )]
 #[get("/{key}")]
 pub async fn get_image_handler(
-    asset_ops: web::Data<AssetUploadOperations>,
+    asset_ops: web::Data<AssetOperations>,
     path: web::Path<(i32,)>,
 ) -> impl Responder {
     let s3_key = path.into_inner().0;

src/api/admin/canteen.rs
@@ -141,7 +141,7 @@ pub(super) async fn get_canteen_menu(
     path: web::Path<(i32,)>,
 ) -> actix_web::Result<impl Responder> {
     let search_canteen_id = path.into_inner().0;
-    let result = web::block(move || menu_ops.get_canteen_items(search_canteen_id)).await?;
+    let result = menu_ops.get_canteen_items(search_canteen_id).await;
     match result {
         Ok(x) => {
             debug!(

src/api/admin/mod.rs
@@ -1,5 +1,5 @@
 use crate::api::ContentTypeHeader;
-use crate::db::{AssetUploadOperations, CanteenOperations, MenuOperations};
+use crate::db::{AssetOperations, CanteenOperations, MenuOperations};
 use actix_web::middleware::NormalizePath;
 use actix_web::web;
 use asset_management::*;
@@ -15,7 +15,7 @@ pub fn config(
     cfg: &mut ServiceConfig,
     menu_ops: &MenuOperations,
     canteen_ops: &CanteenOperations,
-    asset_ops: &AssetUploadOperations,
+    asset_ops: &AssetOperations,
 ) {
     cfg.service(
         scope::scope("/menu")

src/api/common/search.rs
@@ -20,7 +20,7 @@ pub(super) async fn get_search_query_results(
 ) -> actix_web::Result<impl Responder> {
     let search_query = path.into_inner().0;
     let search_query_cl = search_query.clone();
-    let result = web::block(move || search_ops.search_menu_items(&search_query_cl)).await?;
+    let result = search_ops.search_menu_items(&search_query_cl).await;
     match result {
         Ok(x) => {
             debug!(
@@ -64,9 +64,9 @@ pub(super) async fn search_query_by_canteen(
 ) -> actix_web::Result<impl Responder> {
     let (canteen_id, search_query) = path.into_inner();
     let search_query_cl = search_query.clone();
-    let result =
-        web::block(move || search_ops.search_menu_items_by_canteen(&canteen_id, &search_query_cl))
-            .await?;
+    let result = search_ops
+        .search_menu_items_by_canteen(&canteen_id, &search_query_cl)
+        .await;
     match result {
         Ok(x) => {
             debug!(

commit 5174d0eb2b8a998c411804b728201293639a1ca5

retrieve menu pics on /orders
- contd: 500319b
- changes from b3f7ddf introduced too many inconsistencies

Signed-off-by: adithyagenie <adithyagenie@gmail.com>

src/api/common/orders.rs
@@ -130,7 +130,7 @@ pub(super) async fn get_order_by_orderid(
     path: web::Path<(i32,)>,
 ) -> actix_web::Result<impl Responder> {
     let search_order_id = path.into_inner().0;
-    let result = web::block(move || order_ops.get_orders_by_orderid(&search_order_id)).await?;
+    let result = order_ops.get_orders_by_orderid(&search_order_id).await;
     match result {
         Ok(data) => {
             debug!(
@@ -184,7 +184,7 @@ pub(super) async fn get_orders_by_user(
     }
     if let Some(search_user_id) = &params.user_id {
         let search_user_id_cl = *search_user_id;
-        let result = web::block(move || order_ops.get_orders_by_userid(&search_user_id_cl)).await?;
+        let result = order_ops.get_orders_by_userid(&search_user_id_cl).await;
         match result {
             Ok(data) => {
                 debug!(
@@ -214,7 +214,7 @@ pub(super) async fn get_orders_by_user(
         }
     } else if let Some(search_rfid) = &params.rfid {
         let search_rfid_cl = search_rfid.clone();
-        let result = web::block(move || order_ops.get_orders_by_rfid(&search_rfid_cl)).await?;
+        let result = order_ops.get_orders_by_rfid(&search_rfid_cl).await;
         match result {
             Ok(data) => {
                 debug!(

src/db/admin/menu.rs
@@ -88,7 +88,7 @@ impl MenuOperations {
 
         let etag = self
             .asset_ops
-            .get_object_etag(&item_id_to_update.to_string())
+            .get_object_etag(&format!("items/{}", item_id_to_update))
             .await?;
         if etag.is_some() {
             info!("set_menu_item_pic: etag: {}", etag.clone().unwrap());

src/db/common/orders.rs
@@ -1,6 +1,6 @@
-use crate::db::{DbConnection, RepositoryError};
+use crate::db::{AssetOperations, DbConnection, RepositoryError};
 use crate::enums::common::{
-    ActiveItemCount, ItemContainer, OrderItemContainer, TimedActiveItemCount,
+    ActiveItemCount, ItemContainer, OrderItemContainer, OrderItemsWithPic, TimedActiveItemCount,
 };
 use crate::models::common::TimeBandEnum;
 use crate::models::{admin::MenuItemCheck, common::OrderItems, user::NewPastOrder};
@@ -10,6 +10,7 @@ use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::result::Error;
 use diesel::PgConnection;
+use futures::future::join_all;
 use log::{debug, error};
 use std::collections::HashMap;
 
@@ -41,13 +42,16 @@ struct OrderDeliverItems {
 #[derive(Clone)]
 pub struct OrderOperations {
     pool: Pool<ConnectionManager<PgConnection>>,
+    asset_ops: AssetOperations,
 }
 
 impl OrderOperations {
-    pub fn new(pool: Pool<ConnectionManager<PgConnection>>) -> Self {
-        Self { pool }
+    pub async fn new(pool: Pool<ConnectionManager<PgConnection>>) -> Self {
+        Self {
+            pool,
+            asset_ops: AssetOperations::new().await.unwrap(),
+        }
     }
-
     pub fn create_order(
         &self,
         userid: i32,
@@ -277,7 +281,7 @@ impl OrderOperations {
         Ok(resp)
     }
 
-    fn group_order_items(items: Vec<OrderItems>) -> Vec<OrderItemContainer> {
+    fn group_order_items(items: Vec<OrderItemsWithPic>) -> Vec<OrderItemContainer> {
         debug!("Ungrouped order items: {:?}", &items);
         let mut grouped: HashMap<i32, (i32, Option<TimeBandEnum>, Vec<ItemContainer>)> =
             HashMap::new();
@@ -288,11 +292,13 @@ impl OrderOperations {
                 .or_insert_with(|| (item.total_price, item.deliver_at, Vec::new()));
 
             new_item.push(ItemContainer {
-                canteen_name: item.canteen_name,
+                canteen_id: item.canteen_id,
+                item_id: item.item_id,
                 name: item.name,
                 quantity: item.quantity,
                 is_veg: item.is_veg,
                 pic_link: item.pic_link,
+                pic_etag: item.pic_etag,
                 description: item.description,
             });
         }
@@ -315,7 +321,7 @@ impl OrderOperations {
             .collect()
     }
 
-    pub fn get_orders_by_rfid(
+    pub async fn get_orders_by_rfid(
         &self,
         search_rfid: &str,
     ) -> Result<Vec<OrderItemContainer>, RepositoryError> {
@@ -338,13 +344,15 @@ impl OrderOperations {
             .filter(users::rfid.eq(&search_rfid))
             .select((
                 active_orders::order_id,
-                canteens::canteen_name,
+                canteens::canteen_id,
+                menu_items::item_id,
                 active_orders::total_price,
                 active_orders::deliver_at,
                 menu_items::name,
                 active_order_items::quantity,
                 menu_items::is_veg,
                 menu_items::pic_link,
+                menu_items::pic_etag,
                 menu_items::description,
             ))
             .order_by(active_orders::ordered_at.desc())
@@ -362,10 +370,27 @@ impl OrderOperations {
                 }
             })?;
 
-        Ok(Self::group_order_items(order_items))
+        let futures = order_items.iter().map(async |item| {
+            let mut item_with_pic: OrderItemsWithPic = item.into();
+            if item.pic_link {
+                let pic_url = self
+                    .asset_ops
+                    .get_object_presign(&format!("items/{}", &item.item_id.to_string()))
+                    .await
+                    .ok();
+                item_with_pic.pic_link = pic_url;
+                item_with_pic
+            } else {
+                item_with_pic
+            }
+        });
+
+        let results = join_all(futures).await;
+
+        Ok(Self::group_order_items(results))
     }
 
-    pub fn get_orders_by_userid(
+    pub async fn get_orders_by_userid(
         &self,
         search_user_id: &i32,
     ) -> Result<Vec<OrderItemContainer>, RepositoryError> {
@@ -387,13 +412,15 @@ impl OrderOperations {
             .filter(active_orders::user_id.eq(search_user_id))
             .select((
                 active_orders::order_id,
-                canteens::canteen_name,
+                canteens::canteen_id,
+                menu_items::item_id,
                 active_orders::total_price,
                 active_orders::deliver_at,
                 menu_items::name,
                 active_order_items::quantity,
                 menu_items::is_veg,
                 menu_items::pic_link,
+                menu_items::pic_etag,
                 menu_items::description,
             ))
             .order_by(active_orders::ordered_at.desc())
@@ -411,9 +438,26 @@ impl OrderOperations {
                 }
             })?;
 
-        Ok(Self::group_order_items(order_items))
+        let futures = order_items.iter().map(async |item| {
+            let mut item_with_pic: OrderItemsWithPic = item.into();
+            if item.pic_link {
+                let pic_url = self
+                    .asset_ops
+                    .get_object_presign(&format!("items/{}", &item.item_id.to_string()))
+                    .await
+                    .ok();
+                item_with_pic.pic_link = pic_url;
+                item_with_pic
+            } else {
+                item_with_pic
+            }
+        });
+
+        let results = join_all(futures).await;
+
+        Ok(Self::group_order_items(results))
     }
-    pub fn get_orders_by_orderid(
+    pub async fn get_orders_by_orderid(
         &self,
         search_order_id: &i32,
     ) -> Result<OrderItemContainer, RepositoryError> {
@@ -434,13 +478,15 @@ impl OrderOperations {
             .filter(active_order_items::order_id.eq(search_order_id))
             .select((
                 active_order_items::order_id,
-                canteens::canteen_name,
+                canteens::canteen_id,
+                menu_items::item_id,
                 active_orders::total_price,
                 active_orders::deliver_at,
                 menu_items::name,
                 active_order_items::quantity,
                 menu_items::is_veg,
                 menu_items::pic_link,
+                menu_items::pic_etag,
                 menu_items::description,
             ))
             .order(menu_items::item_id.asc())
@@ -458,7 +504,24 @@ impl OrderOperations {
                 }
             })?;
 
-        let resp = Self::group_order_items(order_items);
+        let futures = order_items.iter().map(async |item| {
+            let mut item_with_pic: OrderItemsWithPic = item.into();
+            if item.pic_link {
+                let pic_url = self
+                    .asset_ops
+                    .get_object_presign(&format!("items/{}", &item.item_id.to_string()))
+                    .await
+                    .ok();
+                item_with_pic.pic_link = pic_url;
+                item_with_pic
+            } else {
+                item_with_pic
+            }
+        });
+
+        let results = join_all(futures).await;
+
+        let resp = Self::group_order_items(results);
         Ok(resp.into_iter().next().unwrap_or(OrderItemContainer {
             order_id: *search_order_id,
             total_price: 0,

src/enums/common.rs
@@ -1,3 +1,4 @@
+use crate::models::common::{OrderItems, TimeBandEnum};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use utoipa::ToSchema;
@@ -20,12 +21,47 @@ pub struct TimedActiveItemCountResponse {
 
 #[derive(Serialize, ToSchema, Debug)]
 pub struct ItemContainer {
-    pub canteen_name: String,
+    pub canteen_id: i32,
+    pub item_id: i32,
     pub name: String,
     pub quantity: i16,
     pub is_veg: bool,
-    pub pic_link: bool,
+    pub pic_link: Option<String>,
+    pub pic_etag: Option<String>,
     pub description: Option<String>,
+}
+
+#[derive(Serialize, ToSchema, Debug)]
+pub struct OrderItemsWithPic {
+    pub order_id: i32,
+    pub canteen_id: i32,
+    pub item_id: i32,
+    pub total_price: i32,
+    pub deliver_at: Option<TimeBandEnum>,
+    pub name: String,
+    pub quantity: i16,
+    pub is_veg: bool,
+    pub pic_link: Option<String>,
+    pub pic_etag: Option<String>,
+    pub description: Option<String>,
+}
+
+impl From<&OrderItems> for OrderItemsWithPic {
+    fn from(item: &OrderItems) -> Self {
+        OrderItemsWithPic {
+            order_id: item.order_id,
+            canteen_id: item.canteen_id,
+            item_id: item.item_id,
+            total_price: item.total_price,
+            deliver_at: item.deliver_at.clone(),
+            name: item.name.clone(),
+            quantity: item.quantity,
+            is_veg: item.is_veg,
+            pic_link: None,
+            pic_etag: item.pic_etag.clone(),
+            description: item.description.clone(),
+        }
+    }
 }
 
 #[derive(Serialize, ToSchema, Debug)]

src/main.rs
@@ -35,7 +35,7 @@ impl AppState {
         let user_ops = UserOperations::new(db.clone());
         let menu_ops = MenuOperations::new(db.clone()).await;
         let canteen_ops = CanteenOperations::new(db.clone()).await;
-        let order_ops = OrderOperations::new(db.clone());
+        let order_ops = OrderOperations::new(db.clone()).await;
         let search_ops = SearchOperations::new(db.clone()).await;
         let asset_ops = AssetOperations::new()
             .await

src/models/common.rs
@@ -17,7 +17,7 @@ diesel::allow_columns_to_appear_in_same_group_by_clause!(
 );
 
 #[derive(
-    Debug, PartialEq, FromSqlRow, AsExpression, Eq, Serialize, Deserialize, ToSchema, Hash,
+    Debug, PartialEq, FromSqlRow, AsExpression, Eq, Serialize, Deserialize, ToSchema, Hash, Clone,
 )]
 #[diesel(sql_type = TimeBand)]
 pub enum TimeBandEnum {
@@ -95,12 +95,14 @@ pub struct NewActiveOrder {
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct OrderItems {
     pub order_id: i32,
-    pub canteen_name: String,
+    pub canteen_id: i32,
+    pub item_id: i32,
     pub total_price: i32,
     pub deliver_at: Option<TimeBandEnum>,
     pub name: String,
     pub quantity: i16,
     pub is_veg: bool,
     pub pic_link: bool,
+    pub pic_etag: Option<String>,
     pub description: Option<String>,
 }