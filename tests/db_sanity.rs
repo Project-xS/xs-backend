ee56f7f5b903513bff39869e3a4da19d507bfe8f
adithyagenie <adithyagenie@gmail.com>
2026-02-19 18:51:03 +0530

    test(db): add pg_trgm extension sanity check

--- tests/db_sanity.rs
+++ tests/db_sanity.rs
@@ -26,3 +26,17 @@ fn db_migrations_run_and_empty_state() {
     assert_eq!(canteen_count, 0);
     assert_eq!(menu_item_count, 0);
 }
+
+#[test]
+fn pg_trgm_extension_present() {
+    let pool = common::setup_pool();
+    let mut conn = DbConnection::new(&pool).expect("db connection");
+
+    let ext_count: i64 = diesel::dsl::sql::<diesel::sql_types::BigInt>(
+        "SELECT COUNT(*) FROM pg_extension WHERE extname = 'pg_trgm'",
+    )
+    .get_result(conn.connection())
+    .expect("querying pg_extension should succeed");
+
+    assert_eq!(ext_count, 1, "pg_trgm extension should be installed");
+}
